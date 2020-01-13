825ef835a677c5426e1da4ed25f6b20274950066
codersinghal <48293384+codersinghal@users.noreply.github.com>
2020-01-14 01:28:21 +0530

    Update treediameter.cpp

--- treediameter.cpp
+++ treediameter.cpp
@@ -1,43 +1,57 @@
 /**
  * Description: Find the diameter of the tree.
+ * Using double dfs.
  * Usage: getDiameter O(V + E)
  * Source: https://github.com/dragonslayerx 
  */
 
-typedef vector<vector<pair<int, int> > > tree;
-void dfs(tree &g, int u, int d, bool isVisited[], int dt[]){
-    isVisited[u] = 1;
-    dt[u] = d;
-    for (int i = 0; i < g[u].size(); i++) {
-        int v = g[u][i].first;
-        if (!isVisited[v]) {
-            dfs(g, v, d + g[u][i].second, isVisited, dt);
+#include<bits/stdc++.h>
+using namespace std;
+ 
+vector<vector<int> > g;
+int n;
+ 
+pair<int, int> bfs(int root){
+    vector<int> dist(n+1, 0);
+    dist[root] = 1;
+    queue<int> q;
+    q.push(root);
+    while(!q.empty()){
+        int curr = q.front();
+        for(auto it : g[curr]){
+            if(dist[it] != 0){
+                continue;
+            }
+            q.push(it);
+            dist[it] = dist[curr] + 1;
+        }
+        q.pop();
+    }
+    int mx = INT_MIN, vertex = -1;
+    for(int i=1 ; i<=n ; i++){
+        if(mx < dist[i]){
+            mx = dist[i];
+            vertex = i;
         }
     }
+    return make_pair(mx, vertex);
 }
-
-const int MAX = 100005;
-bool isVisited[MAX];
-int dt[MAX];
-
-int getDiameter(tree &g) {
-	memset(dt, 0, sizeof(dt));
-	memset(isVisited, 0, sizeof(isVisited));
-	dfs(g, 0, 0, isVisited, dt);
-	int max_dist = 0, max_pos = 0;
-	int n = g.size();
-	for (int i = 0; i < n; i++) {
-		if (max_dist < dt[i]) {
-            		max_dist = dt[i];
-            		max_pos = i;
-        	}
-    	}
-	memset(dt, 0, sizeof(dt));
-    	memset(isVisited, 0, sizeof(isVisited));
-	dfs(g, max_pos, 0, isVisited, dt);
-	max_dist = 0;
-    	for (int i = 0; i < n; i++) {
-        	max_dist = max(max_dist, dt[i]);
-    	}
-	return max_dist;
+ 
+int diameter(int root){
+    pair<int, int> p1 = bfs(root), p2;
+    p2 = bfs(p1.second);
+    return p2.first;
+}
+ 
+int main(){
+    cin >> n;
+    g.resize(n+1);
+    for(int i=0, u, v ; i<n-1 ; i++){
+        cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+// choosing arbitrary node to be 1.
+    cout << diameter(1) ;
+    return 0;
 }
