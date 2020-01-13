b0683feeeca338e727290e30d6032a5c0808afcf
codersinghal <48293384+codersinghal@users.noreply.github.com>
2020-01-14 01:29:46 +0530

    Update toposort.cpp

--- toposort.cpp
+++ toposort.cpp
@@ -4,41 +4,80 @@
  * Source: https://github.com/dragonslayerx 
  */
 
-const int MAX = 10050;
+#include <bits/stdc++.h>
+using namespace std;
 
-bool isvisited[MAX];
-vector<int> s;
-vector<vector<int> > g;
+#define SIZE 100000
 
-void dfs(int u)
-{
-    isvisited[u] = 1;
-    for (int i = 0; i < g[u].size(); i++) {
-        int v = g[u][i];
-        if (!isvisited[v]) {
-            dfs(v);
-        }
-    }
-    s.push_back(u);
+vector<int> graph[SIZE];
+
+int visited[SIZE] = {0};
+stack<int> topoOrder;
+void toposort(int node) {
+	visited[node] = 1;
+	for(int adj: graph[node]) {
+		if(!visited[adj]) {
+			toposort(adj);
+		}
+	}
+	topoOrder.push(node);
 }
 
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    g.resize(n);
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--, b--;
-        g[a].push_back(b);
-    }
-    for (int i = 0; i < n; i++) {
-        if (!isvisited[i]) dfs(i);
-    }
-    reverse(s.begin(), s.end());
-    for (int i = 0; i < s.size(); i++) {
-        cout << s[i]+1 << " ";
-    }
-    cout << endl;
+int main() {
+	int n, m;
+	int indegree[SIZE] = {0};
+	// here n is no of nodes and m is no of edges
+	cin>>n>>m;
+
+	// taking directed edges as input
+	for(int i=0;i<m;i++) {
+		int u, v;
+		cin>>u>>v;
+		graph[u].push_back(v);
+		indegree[v]++;
+	}
+
+	// toposort using dfs
+	printf("Using dfs, Topological Sort: \n");
+	for(int i=1;i<=n;i++) {
+		// we need to call dfs for all disconnected components if present
+		if(!visited[i]) {
+			toposort(i);
+		}
+	}
+	while(!topoOrder.empty()) {
+		printf("%d ", topoOrder.top());
+		topoOrder.pop();
+	}
+	printf("\n");
+
+	// toposort using indegree approach
+	printf("Using in-degree method, Topological Sort: \n");
+	queue<int> q;
+	vector<int> topoOrder2;
+	for(int i=1;i<=n;i++) {
+		if(indegree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	while(!q.empty()) {
+		int curr = q.front();
+		topoOrder2.push_back(curr);
+		q.pop();
+
+		for(int adj: graph[curr]) {
+			indegree[adj]--;
+			if(indegree[adj] == 0) {
+				q.push(adj);
+			}
+		}
+	}
+	for(int node: topoOrder2) {
+		printf("%d ", node);
+	}
+	printf("\n");
+
+
+	return 0;
 }
