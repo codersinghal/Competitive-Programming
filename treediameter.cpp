8c41d740576bb48a8b16fd571132efbc6d49ef5c
harshitkapoor7 <59888918+harshitkapoor7@users.noreply.github.com>
2020-10-01 11:27:31 +0530

    Added an approach for trees made using structures and pointers.

--- treediameter.cpp
+++ treediameter.cpp
@@ -7,6 +7,23 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
+
+ 
+//       An approach for structured trees
+    /*int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+    public int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L+R+1);
+        return Math.max(L, R) + 1;
+    }*/
  
 vector<vector<int> > g;
 int n;
